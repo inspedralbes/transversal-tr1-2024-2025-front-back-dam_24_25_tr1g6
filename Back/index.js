bd53f2db2219aa0d19ddb327a9d2e23e1d3f6d6d
Martí <a23marclacas@inspedralbes.cat>
2024-10-22 12:31:36 +0200

    GetProductesJson Funcional, Get BD en procés 22/10/24

--- Back/index.js
+++ Back/index.js
@@ -1,1 +1,40 @@
 "use strict";
+const express = require('express');
+const app = express();
+const mysql = require('mysql2/promise');
+const PORT = 3000;
+app.use(express.json());
+
+
+// Llegir el fitxer JSON amb els productes
+app.get('/getProductesJson', (req, res) => {
+    let productes = require('./db/Productes.json');
+    res.json(productes);
+});
+
+// TODO: Llegir els productes de la Base de Dades
+app.get('/getProductesBD', (req, res) => {
+    
+    mysql.createConnection({
+    host: 'dam.inspedralbes.cat',
+    user: 'a21rublormar_admin',
+    password: 'InsPedralbes2024',
+    database: 'a21rublormar_TR1_GR6'
+    })
+    .then(connection => {
+        console.log("Connexió creada")
+        return connection.execute('SELECT * FROM productes')
+        .then(([resultats, camps]) => {
+            console.log(resultats);
+        })
+        .finally(() => connection.end());
+    })
+    .catch(err => {
+        console.error('Error: ', err);
+    });
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor en funcionament a http://localhost:${PORT}`);
+});
